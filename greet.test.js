7a9ae5d7e58c72f62cd1fd5c0a2200eae591566a
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-03-23 09:52:13 +0200

    Delete greet.test.js

--- greet.test.js
+++
@@ -1,9 +0,0 @@
-describe('The greet function' , function(){
-    it('should show get name correctly' , function(){
-        assert.equal('Hello, Name', greet('Name'));
-    });
-    it('should greet Kagiso correclty', function(){
-        assert.equal('Hello, Kagiso', greet('Kagiso'));
-    });
-
-});

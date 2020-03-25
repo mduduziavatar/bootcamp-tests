bc23594e75188cd183f623de9108e2891dc57278
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-03-25 21:43:50 +0200

    commit changes

--- test/transportFee.test.js
+++ test/transportFee.test.js
@@ -1,8 +1,13 @@
-describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
+describe("The transportFee function", function(){
+    it("should return free for night shift", function(){
+        assert.equal("free", transportFee("free"));
     });
+    it("should return R20 for morining", function(){
+        assert.equal("R20", transportFee("morning"));
+    });
+    it("should return R10 for afternoon", function(){
+        assert.equal("R10", transportFee("afternoon"));
+    });
+  
 
 });

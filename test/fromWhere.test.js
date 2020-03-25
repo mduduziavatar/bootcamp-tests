335ac960424374f79e8df057c67dacde56142a43
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-03-25 22:50:58 +0200

    commit changes

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -1,8 +1,17 @@
-describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
+describe("The fromWhere function" , function(){
+    it("should return reg numbers from Bellville as CY" , function(){
+        assert.equal("Bellville", fromWhere("CY"));
+    });
+    it("should return reg numbers from Cape Town as CA" , function(){
+        assert.equal("Cape Town", fromWhere("CA"));
+    });
+    it("should return reg numbers from Paarl as CJ" , function(){
+        assert.equal("Paarl", fromWhere("CJ"));
+    });
+    it("should return reg numbers from some other place" , function(){
+        assert.equal("Some other place!", fromWhere(""));
     });
 
 });
+;
+

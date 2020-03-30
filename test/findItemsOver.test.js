2a4aca0cccd43110298029ee4601905d36242b68
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-03-30 15:37:02 +0200

    added local files

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,8 +1,13 @@
-describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
+describe("The findItemsOver function", function (itemList, threshold) {
+    it("should find items with quantity over", function () {
+        var threshold = 30;
+        var itemList = [
+        { name: "apples", qty: 10 },
+        { name: "pears", qty: 37 },
+        { name: "bananas", qty: 27 },
+        { name: "apples", qty: 3 }
+        ];
+        assert.deepEqual([{ name: "pears", qty: 37 }], findItemsOver(itemList, threshold));
 
-        assert.deepEqual([2,2],[2,2]);
     });
-
 });
--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,8 +1,11 @@
-describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
+describe("The findItemsOver20 function", function (list) {
+    it("should find items with quantity over 20", function () {
+        var list = [
+            { name: "apples", qty: 10 },
+            { name: "pears", qty: 37 },
+            { name: "bananas", qty: 27 },
+            { name: "apples", qty: 3 }
+        ];
+        assert.deepEqual([{ name: "pears", qty: 37 }, { name: "bananas", qty: 27 }], findItemsOver20(list));
     });
-
 });
